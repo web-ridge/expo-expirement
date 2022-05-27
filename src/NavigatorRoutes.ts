54109e022c012d59361c321a0fec56f53bc25823
Richard Lindhout <richardlindhout96@gmail.com>
2022-05-27 22:27:37 +0200

    Expo expirement config linking

---
+++ src/NavigatorRoutes.ts
@@ -0,0 +1,5 @@
+import * as r from './NavigatorScreens';
+
+// make autocomplete and auto-import works in the IDE when typing routes
+const routes = r;
+export default routes;
---
+++ src/Post/PostScreen.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Paragraph } from "react-native-paper";
+
+import { useParams } from "react-native-ridge-navigation";
+
+import { useQuery } from "react-query";
+import routes from "../NavigatorRoutes";
+import { ScrollView } from "react-native";
+import Header from "../Header";
+import { queryKeyPostScreen, queryKeyPostScreenPromise } from "../queryKeys";
+
+export default function PostScreen(props: any) {
+  const { id } = useParams(routes.PostScreen, props);
+  const { data } = useQuery(
+    queryKeyPostScreen({ id }),
+    queryKeyPostScreenPromise({ id })
+  );
+
+  return (
+    <>
+      <Header title={data!.title} withBack />
+      <ScrollView style={{ padding: 12 }}>
+        <Paragraph>{data!.body}</Paragraph>
+      </ScrollView>
+    </>
+  );
+}
