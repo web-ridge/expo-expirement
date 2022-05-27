f0b7f860ffa4546ccf5eeb677e339e9debe2a730
Richard Lindhout <richardlindhout96@gmail.com>
2022-05-27 23:09:46 +0200

    more examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   DefaultTheme,
   DarkTheme,
   Provider as PaperProvider,
-} from 'react-native-paper';
+} from "react-native-paper";
 import {
   LogBox,
   Platform,
   ColorSchemeName,
   useColorScheme,
-} from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+} from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import AsyncBoundary from './helpers/AsyncBoundary';
-import LimitedView from './helpers/LimitedView';
+import AsyncBoundary from "./helpers/AsyncBoundary";
+import LimitedView from "./helpers/LimitedView";
 
-import { QueryClientProvider } from 'react-query';
-import queryClient from './queryClient';
+import { QueryClientProvider } from "react-query";
+import queryClient from "./queryClient";
 
 function getTheme(colorScheme: ColorSchemeName): typeof DarkTheme {
-  const isDark = colorScheme === 'dark';
-  const baseTheme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
+  const isDark = colorScheme === "dark";
+  const baseTheme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
 
   return {
     ...baseTheme,
-    mode: 'adaptive',
+    mode: "adaptive",
     dark: isDark,
     roundness: 10,
   };
 }
 
 export default function AppHOC<T extends object>(
-  WrappedComponent: React.ComponentType<T>
+  WrappedComponent: any
 ): React.FC<T> {
   function Wrapper(wrapperProps: T) {
     const colorScheme = useColorScheme(); // Can be dark | light | no-preference
     const memoTheme = React.useMemo(() => getTheme(colorScheme), [colorScheme]);
     React.useEffect(() => {
-      if (Platform.OS === 'android') {
-        LogBox.ignoreLogs(['Setting a timer']);
+      if (Platform.OS === "android") {
+        LogBox.ignoreLogs(["Setting a timer"]);
       }
     }, []);
     return (
@@ -54,7 +54,7 @@ export default function AppHOC<T extends object>(
               </AsyncBoundary>
             </LimitedView>
           </PaperProvider>
-          {Platform.OS === 'web' ? (
+          {/* {Platform.OS === 'web' ? (
             <style type="text/css">{`
                       @font-face {
                         font-family: 'MaterialCommunityIcons';
@@ -64,7 +64,7 @@ export default function AppHOC<T extends object>(
                         }) format('truetype');
                       }
                     `}</style>
-          ) : null}
+          ) : null} */}
         </SafeAreaProvider>
       </QueryClientProvider>
     );
